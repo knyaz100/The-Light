0908c8660001988b6fd63a77d4284d65457fa0f9
Vasily Churbanov <vasily.churbanov@gmail.com>
2021-01-27 23:44:51 +0300

    1.0

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    var isLightOn = false
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()        
+        updateUI()
     }
 
-
+    fileprivate func updateUI() {
+        view.backgroundColor = isLightOn ? .white : .black
+    }
+    
+    fileprivate func switchFlash(isOn: Bool) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+        
+        do {
+            try device.lockForConfiguration()
+            
+            if (!isOn) {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            } else {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                } catch {
+                    print(error)
+                }
+            }
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func buttonPressed() {
+        
+        isLightOn.toggle()
+        updateUI()
+        switchFlash(isOn: isLightOn)
+        
+    }
+    
+    
 }
 
